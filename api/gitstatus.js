9ce37e6eed1044a2fa19a2f738de817dcd85af0a
Viktor <fullzero5@gmail.com>
2019-10-03 19:16:21 +0000

    create api

---
+++ api/gitstatus.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = async () => [
+    'The git status command displays the state of the working directory and the staging area',
+    'Status output does not show you any information regarding the committed project history'
+];
